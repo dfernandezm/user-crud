6d9b21013479e16c54767637d9d4b36b13c80697
davidfernandezm <morenza@gmail.com>
2013-09-16 09:58:28 +0200

    Changed comments in test classes

--- user-crud/src/main/webapp/view/js/extjs/app.js
+++ user-crud/src/main/webapp/view/js/extjs/app.js
@@ -2,7 +2,8 @@
  * Initial JS stub files to implement the front end with ExtJS 4 MVC approach. Powerful, rich and clean way of implement a 
  * management-like front end with lots of built-in widgets and interactions 
  * 
- * */
+ * 
+ */
 
 Ext.require('Ext.container.Viewport');
 
--- user-crud/src/test/java/com/kuluvalley/usercrud/test/UserServiceTest.java
+++ user-crud/src/test/java/com/kuluvalley/usercrud/test/UserServiceTest.java
@@ -1,10 +1,11 @@
 package com.kuluvalley.usercrud.test;
 
 /**
- * A class with JUnit-based test to perform integration test for the service layer
+ * A class with JUnit-based test to perform integration tests for the service layer. It is important to automate these tests in order to 
+ * be sure everything is working properly in any stage of the development process.
  * 
  * 
- * 1. Check correct creation -> force expected errors, force unexpected errors
+ * - Check correct creation -> force expected errors, force unexpected errors
  * 
  * @author David
  *
